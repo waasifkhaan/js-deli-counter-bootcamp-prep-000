ee31a36766982f372fc71acb44ac5b94fc4e095e
Wasif Khan <waasif.khaan@gmail.com>
2018-04-14 22:01:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
 }
 function nowServing(katzDeliLine){
   if( katzDeliLine.length = 0){
-    return "There is nobody waiting to be served!"  
+    return "There is nobody waiting to be served!"
   }
   else{
     return `Currently serving ${katzDeliLine[0]}.`
     katzDeliLine.splice(1);
-    
+
   }
   }
 
@@ -21,5 +21,5 @@ function nowServing(katzDeliLine){
       `The line is currently: ${katzDeliLine}`
     }
 
-    
+
   }

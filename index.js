ef7c80d18f53a20b75514df0d36375bd806d26e2
Wasif Khan <waasif.khaan@gmail.com>
2018-04-14 21:41:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,8 +4,7 @@ return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
 }
 function nowServing(katzDeliLine){
   if( katzDeliLine.length > 0){
-    katzDeliLine.splice(1);
-    return `Currently serving ${katzDeliLine[0]}.`
+      return `Currently serving ${katzDeliLine[0]}.`;   katzDeliLine.splice(1);
 
 
   }

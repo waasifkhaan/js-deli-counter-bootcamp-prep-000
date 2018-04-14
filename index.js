b747e6a75a0d81287d60da13df20fc1c95a70cf8
Wasif Khan <waasif.khaan@gmail.com>
2018-04-14 21:20:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,13 @@ function takeANumber(katzDeliLine, name) {
 katzDeliLine.push(name);
 return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
 }
+function nowServing(katzDeliLine){
+  if( katzDeliLine.length > 0){
+    return katzDeliLine[0];
+    katzDeliLine.shift()  
+  }
+  
+  else{
+    return "There is nobody waiting to be served!"
+  }
+  }

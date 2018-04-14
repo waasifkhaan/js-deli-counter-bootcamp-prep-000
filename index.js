c4a4644b6930581a5bba6842558fde8d5f6ea322
Wasif Khan <waasif.khaan@gmail.com>
2018-04-14 21:09:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,4 @@
 function takeANumber(katzDeliLine, name) {
-var katzDeli = [];
-katzDeli.push(name);
-for(let i = 0; i<katzDeli.length ; i++){
-`${katzDeli[i]}` = name
-}
-return `"Welcome, ${name}. You are number ${i} in line."`
+katzDeliLine.push(name);
+return `Welcome, ${name}. You are number ${katzDeliLine.length}  in line.`
 }
